01fdf90965bb425138dd5e4682c98f80836dc584
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-01-15 10:42:18 +0600

    first commit

---
+++ Item.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesLession
+{
+    public class Item
+    {
+        //autoproperty (автосвойство)
+        //public возвр_тип Название { мод_доступа get; мод_доступа set; }
+
+        public int Id { get; set; }
+
+        //property (свойство)
+        //public возвр_тип Название {
+        //      мод_доступа get {
+        //           тело;
+        //           return переменная;
+        //      }
+        //      мод_доступа set {
+        //           тело;
+        //           переменная = value;
+        //      }
+        // }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public double Price { get; set; }
+
+        public DateTime CreationDate { get; private set; }
+        public DateTime[] ChangedDates { get; set; }
+        public DateTime this[int index]
+        {
+            get
+            {
+                return ChangedDates[index];
+            }
+            set
+            {
+                ChangedDates[index] = value;
+            }
+        }
+
+
+        public Item()
+        {
+            CreationDate = DateTime.Now;
+        }
+
+
+
+        /*private int _id;
+        private string _name;
+        private double _price;
+
+        public int GetId()
+        {
+            return _id;
+        }
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+        */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesLession
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item();
+            item.Name = "Хлеб"; //вызван set
+            string itemName = item.Name; //вызван get
+            Console.WriteLine(item.Name); //тоже get
+
+            Item anotherItem = new Item
+            {
+                Id = 1,
+                Name = "Носки",
+                Price = 100,
+                //CreationDate = DateTime.Now эта строчка не нужна, чтобы каждый раз при создании не дублировать код
+            };
+
+            anotherItem.ChangedDates = new DateTime[2];
+            anotherItem[0] = DateTime.Now;
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            numbers[0] = 12; //array[] - ?
+
+            Array.Resize(ref numbers, 30);
+        }
+    }
+}
